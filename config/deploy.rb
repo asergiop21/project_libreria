eb98043644afdeb631eb8d2dbbae17c8e351f2fd
Sergio <asergiop21@hotmail.com>
2015-02-10 18:37:08 -0300

    cambio git

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'project'
-set :repo_url, 'git@github.com:asergiop21/project.git'
+set :repo_url, 'git@github.com:asergiop21/project_libreria.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
